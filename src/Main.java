a8e9eb6e88c980a60c60c127b836ae5475c83008
Makx3 <gatoconb42@gmail.com>
2023-08-24 09:54:35 -0400

    commit de prueba

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        system.out.println("Ingrese el numero de filas:");
+        int m= teclado.nextInt();
+        system.out.println("Ingrese el numero de columnas:");
+        int m = teclado.nextInt();
+
+    }
+    public static int [][] crearmatrizvacia(int m,int n){
+        int [][] matriz= new int[m][n];
+        return matriz;
+    }(
+}
