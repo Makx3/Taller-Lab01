971aec747e33012e276be72ce52a6cf937042aa7
Makx3 <gatoconb42@gmail.com>
2023-08-24 10:23:11 -0400

    commit de prueba

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        system.out.println("Ingrese el numero de filas:");
-        int m= teclado.nextInt();
-        system.out.println("Ingrese el numero de columnas:");
-        int m = teclado.nextInt();
 
+        int m, n;
+        do {
+            System.out.println("Ingrese el numero de filas:");
+            m = teclado.nextInt();
+        } while (m <= 0);
+
+        do {
+            System.out.println("Ingrese el numero de columnas:");
+            n = teclado.nextInt();
+        } while (n <= 0);
+
+        int[][] matriz = crearmatrizvacia(m, n);
+
+        System.out.println("Matriz vacía:");
+        imprimirMatriz(matriz);
     }
-    public static int [][] crearmatrizvacia(int m,int n){
-        int [][] matriz= new int[m][n];
+
+    public static int[][] crearmatrizvacia(int m, int n) {
+        int[][] matriz = new int[m][n];
         return matriz;
-    }(
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
